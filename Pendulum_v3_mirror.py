954099ee165a76ada06f49a8e62c081137db1ac5
drib861204 <56364972+drib861204@users.noreply.github.com>
2022-06-29 01:19:10 +0800

    0629

---
+++ Pendulum_v3_mirror.py
@@ -0,0 +1,253 @@
+"""
+Title: pendulum_pygame
+Author: [jadenhensley](https://github.com/jadenhensley)
+Last modified: 2021/10/18
+Description: Pendulum project, built using pygame and math modules.
+
+Title: wheelPole
+Author: [aimetz](https://github.com/aimetz)
+Last modified: 2021/04/20
+
+Title: gym/gym/envs/classic_control/pendulum.py
+Author: [openai](https://github.com/openai)
+Last modified: 2021/10/31
+"""
+import pygame
+from math import pi, sin, cos
+import numpy as np
+
+import gym
+from gym import spaces, logger
+
+
+class Pendulum(gym.Env):
+    def __init__(self, rend):
+        #self.frames = frames
+        #self.interval_num = interval_num
+        #self.cur_case = 1
+        #self.weight_tau = w_tau
+
+        self.theta_rod = 0
+        self.theta_wheel = 0
+        self.theta_rod_dot = 0
+        self.theta_wheel_dot = 0
+        self.len_rod = 0.5
+        self.len_wheel = 0.5
+        self.mass_rod = 20
+
+        #self.rad_out = 0.05
+        self.rad_out = 0.13# 0.105
+        self.rad_in = 0.065
+        #self.t = 0.01
+        self.t = 0.03
+        self.rho = 7870
+        self.mass_wheel = 5.0295
+        self.momentum_wheel = 0.0654
+        #self.mass_wheel = (self.rad_out**2-self.rad_in**2)*self.t*pi*self.rho
+        #self.momentum_wheel = self.mass_wheel*(self.rad_out**2+self.rad_in**2)/2
+
+        #print(self.mass_wheel)
+        #print(self.momentum_wheel)
+
+        self.momentum_rod = 0.95
+        self.dt = 0.005
+        self.gravity = 9.81
+        self.max_q1 = 3.5*pi/180 # stop training below this angle
+        self.max_q1dot = 1 #initial q1_dot default 0.3? to be verified
+
+        self.wheel_max_speed = 28
+        self.max_torque = 21
+
+        self.torque = 0
+        self.last_torque = 0
+
+        self.Ip = self.mass_rod*self.len_rod**2+self.mass_wheel*self.len_wheel**2+self.momentum_rod+self.momentum_wheel
+        self.mbarg = (self.mass_rod*self.len_rod+self.mass_wheel*self.len_wheel)*self.gravity
+
+        high = np.array([self.max_q1, self.max_q1dot, self.wheel_max_speed], dtype=np.float32)
+        low = np.array([0, -self.max_q1dot, -self.wheel_max_speed], dtype=np.float32)
+        self.action_space = spaces.Box(low=-self.max_torque, high=self.max_torque, shape=(1,), dtype=np.float32)
+        self.observation_space = spaces.Box(low=low, high=high, dtype=np.float32)
+
+        #print(self.mass_wheel)
+        #print(self.momentum_rod)
+        #print(self.momentum_wheel)
+
+        width = 800
+        height = 600
+        self.origin_x = width//2
+        self.origin_y = height//2
+        self.POS = np.array([self.origin_x, self.origin_y])
+
+        if rend:
+            pygame.init()
+            self.screen = pygame.display.set_mode((width, height))
+            pygame.display.set_caption("Pendulum Simulation")
+            pygame.font.init()
+            self.debug_font = pygame.font.SysFont('Bauhuas 93', 30)
+            self.hint_font = pygame.font.SysFont('Bauhaus 93', 26)
+            #print("font")
+
+
+    def reset(self, mode, avg_reward=-1000):
+        # self.state is for render, self.agent_state is for training
+
+        self.ang = 2*pi/180 # reset angle
+        #self.low_ang = 0 # random lower bound
+
+        #if saved == None:
+        if mode == "train":
+            #interval = self.frames//self.interval_num
+            '''if avg_reward > -50:
+                self.cur_case += 1
+            self.ang *= (self.cur_case)/self.interval_num
+            #print(interval, self.ang)'''
+
+            reset_angle_random = np.random.uniform(low=-self.ang, high=self.ang)
+            #reset_high = np.array([self.ang, self.max_q1dot, self.wheel_max_speed])
+            #self.state = np.random.uniform(low=-reset_high, high=reset_high)
+            self.state = np.array([reset_angle_random, 0, 0], dtype=np.float32)
+            self.agent_state = np.array([abs(reset_angle_random), 0, 0], dtype=np.float32)
+        #else:
+        elif mode == "test":
+            self.state = np.array([self.ang, 0, 0], dtype=np.float32)
+            self.agent_state = np.array([abs(self.ang), 0, 0], dtype=np.float32)
+            # self.state = np.array([0, self.max_q1dot, 0],dtype=np.float32)
+
+        #self.last_u = None
+        self.last_torque = 0
+
+        return self.agent_state
+
+
+    def render(self, eval_run):
+        #torque = action
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+
+        SCALE = 200
+        BLACK = (0, 0, 0)
+        WHITE = (255, 255, 255)
+        RED = (255, 0, 0)
+        GRAY = (128, 128, 128)
+
+        tip_x = self.POS[0]+self.len_wheel*sin(self.theta_rod)*SCALE
+        tip_y = self.POS[1]-self.len_wheel*cos(self.theta_rod)*SCALE
+        POSTIP = np.array([tip_x, tip_y])
+        POSWHEEL = np.array(([tip_x+self.rad_out*sin(self.theta_wheel)*SCALE/4, tip_y-self.rad_out*cos(self.theta_wheel)*SCALE/4]))
+        #print(POSTIP)
+        self.screen.fill(WHITE)
+        pygame.draw.line(self.screen, BLACK, self.POS, POSTIP, 10)
+        pygame.draw.circle(self.screen, GRAY, POSTIP, self.rad_out*2*SCALE/4)
+        pygame.draw.circle(self.screen, RED, POSWHEEL, self.rad_out*2*SCALE/5/4)
+        img = self.hint_font.render("torque  : % .4f" %self.torque, True, BLACK)
+        #img2 = self.hint_font.render("voltage: % .4f" %self.voltage, True, BLACK)
+        img3 = self.hint_font.render("Evaluation Run %d" %eval_run, True, BLACK)
+        self.screen.blit(img, (self.origin_x, self.origin_y/2-50))
+        #self.screen.blit(img2, (self.origin_x, self.origin_y/2-30))
+        self.screen.blit(img3, (self.origin_x/5, self.origin_y/2-50))
+
+        pygame.display.update()
+
+
+    def step(self, action):
+        q1, q1_dot, q2_dot = self.state
+        q1 = angle_normalize(q1)
+
+        #q1 = self.theta_rod
+        q2 = self.theta_wheel
+        #q1_dot = self.theta_rod_dot
+        #q2_dot = self.theta_wheel_dot
+        #l1 = self.len_rod
+        #l2 = self.len_wheel
+        #m1 = self.mass_rod
+        #m2 = self.mass_wheel
+        #I1 = self.momentum_rod
+        I2 = self.momentum_wheel
+        dt = self.dt
+        #g = self.gravity
+        action_scale = self.max_torque
+
+        if q1 >= 0:
+            torque = action * action_scale
+        else:
+            torque = -action * action_scale
+        torque = torque[0]
+
+        #if q2_dot >= self.wheel_max_speed or q2_dot <= -self.wheel_max_speed:
+        #    torque = 0
+
+        Ip = self.Ip
+        a = self.mbarg*sin(q1)
+
+        if abs(q2_dot) <= self.wheel_max_speed:
+            q1_dot = q1_dot + ((a - torque) / (Ip - I2)) * dt
+            q2_dot = q2_dot + ((torque * Ip - a * I2) / I2 / (Ip - I2)) * dt
+        #elif abs(q2_dot) > self.wheel_max_speed:
+        #    q1_dot = q1_dot
+        #    q2_dot = q2_dot
+        q1 = angle_normalize(q1 + q1_dot * dt)
+        #q2_dot = np.clip(q2_dot, -self.wheel_max_speed, self.wheel_max_speed)
+        q2 = angle_normalize(angle_normalize(q2) + q2_dot * dt)
+
+        #state = np.array([q1[0], q1_dot[0], q2_dot[0]], dtype=np.float32)
+        self.state = (q1, q1_dot, q2_dot)
+        if q1 >= 0:
+            self.agent_state = self.state
+        else:
+            self.agent_state = (-self.state[0], -self.state[1], -self.state[2])
+
+        done = bool(
+            q1 < -self.max_q1
+            or q1 > self.max_q1
+        )
+            #or q1_dot < -self.max_q1dot
+            #or q1_dot > self.max_q1dot
+
+        self.theta_rod = q1
+        self.theta_wheel = q2
+        self.theta_rod_dot = q1_dot
+        self.theta_wheel_dot = q2_dot
+        self.torque = torque
+
+        # costs = 100 * q1 ** 2
+        # costs = q1_dot ** 2
+        # costs = 100 * q1 ** 2 + 1 * q1_dot ** 2
+        costs = 100 * q1 ** 2 + 1 * q1_dot ** 2 + 0.0001 * (self.last_torque - torque) ** 2
+        # costs = 100 * q1 ** 2 + 1 * q1_dot ** 2 + 0.0001 * (self.last_torque - torque) ** 2 + self.weight_speed * q2_dot**2
+        # costs = 100 * q1 ** 2 + 1 * q1_dot ** 2 + 0.0001 * (self.last_torque - torque) ** 2 + self.weight_tau * torque**2
+        # costs = 100 * q1 ** 2 + 1 * q1_dot ** 2 + self.weight_tau * torque ** 2
+        # costs = 1000 * q1 ** 2 + 0.1 * q1_dot ** 2 + 0.001 * torque ** 2 + 0.00001 * q2_dot**2
+        # costs = 100 * q1 ** 2 + 0.00001 * q2_dot ** 2
+        # costs = 100 * q1 ** 2 + 1 * q1_dot ** 2 + 100 * torque ** 2 + 0.001 * q2_dot ** 2
+
+        if done:
+            costs += 100
+
+        #if abs(q1) < 0.001 and abs(q1_dot) < 0.001 and abs(q2_dot) < 0.1 :
+        #    costs -= 1000
+        #elif abs(q1) < 0.001 and abs(q1_dot) < 0.001 and abs(q2_dot) < 0.01 :
+        #    costs -= 1000
+
+        '''if q1 > 0.01 or q1 < -0.01 or q1_dot > 0.05 or q1_dot < -0.05:
+            costs = 100 * q1 ** 2 + 1 * q1_dot ** 2 + 0.0001 * (self.last_torque - torque) ** 2
+        else:
+            costs = 0.000025 * q2_dot ** 2 + 0.0001 * (self.last_torque - torque) ** 2'''
+
+        self.last_torque = torque
+
+        #print("state: ", self.state)
+        #print("agent state: ", self.agent_state)
+
+        #return state, -costs, False, {}
+        return np.array(self.agent_state, dtype=np.float32), -costs, done, {}
+
+    def close(self):
+        pygame.display.quit()
+        pygame.quit()
+
+
+def angle_normalize(th):
+    return ((th+pi)%(2*pi))-pi
---
+++ train.py
@@ -0,0 +1,208 @@
+import os
+import time
+from datetime import datetime
+import torch
+import numpy as np
+import argparse
+from collections import deque
+from Pendulum_v3_mirror import *
+from files.Agent import Agent
+import utils
+import TD3
+from PPO import PPO
+from test import test
+
+
+parser = argparse.ArgumentParser(description="")
+parser.add_argument("-type", type=str, default=None, help="SAC, TD3, PPO")
+parser.add_argument("-trial", type=int, default=0, help="trial")
+parser.add_argument("-seed", type=int, default=0, help="Seed for the env and torch network weights, default is 0")
+parser.add_argument("-frames", type=int, default=1e5, help="frames")
+parser.add_argument("-w_q1", type=int, default=10, help="q1 weight")
+parser.add_argument("-lr_a", type=float, default=0.0003, help="learning rate for actor network")
+parser.add_argument("-lr_c", type=float, default=0.001, help="learning rate for critic network")
+parser.add_argument("-r", "--render", type=int, default=0, choices=[0, 1], help="Rendering the evaluation runs if set to 1, default=0")
+
+#SAC parameters
+parser.add_argument("-per", type=int, default=0, choices=[0, 1],
+                    help="Adding Priorizied Experience Replay to the agent if set to 1, default = 0")
+parser.add_argument("-munchausen", type=int, default=0, choices=[0, 1],
+                    help="Adding Munchausen RL to the agent if set to 1, default = 0")
+parser.add_argument("-dist", "--distributional", type=int, default=0, choices=[0, 1],
+                    help="Using a distributional IQN Critic if set to 1, default=0")
+parser.add_argument("-ere", type=int, default=0, choices=[0, 1],
+                    help="Adding Emphasizing Recent Experience to the agent if set to 1, default = 0")
+parser.add_argument("-n_step", type=int, default=1, help="Using n-step bootstrapping, default=1")
+parser.add_argument("-d2rl", type=int, choices=[0, 1], default=0,
+                    help="Uses Deep Actor and Deep Critic Networks if set to 1 as described in the D2RL Paper: https://arxiv.org/pdf/2010.09163.pdf, default=0")
+parser.add_argument("-bs", "--batch_size", type=int, default=256, help="Batch size, default is 256")
+parser.add_argument("--n_updates", type=int, default=1,
+                    help="Update-to-Data (UTD) ratio, updates taken per step with the environment, default=1")
+parser.add_argument("-repm", "--replay_memory", type=int, default=int(1e6),
+                    help="Size of the Replay memory, default is 1e6")
+parser.add_argument("-g", "--gamma", type=float, default=0.99, help="discount factor gamma, default is 0.99")
+parser.add_argument("-w", "--worker", type=int, default=1, help="Number of parallel worker, default = 1")
+parser.add_argument("-t", "--tau", type=float, default=0.005, help="Softupdate factor tau, default is 0.005")
+parser.add_argument("-layer_size", type=int, default=256,
+                    help="Number of nodes per neural network layer, default is 256")
+parser.add_argument("-a", "--alpha", type=float,
+                    help="entropy alpha value, if not choosen the value is leaned by the agent")
+
+#TD3 parameters
+parser.add_argument("--start_timesteps", default=25e3, type=int)  # Time steps initial random policy is used
+parser.add_argument("--expl_noise", default=0.1, type=float)  # Std of Gaussian exploration noise
+parser.add_argument("--policy_noise", default=0.2, type=float)  # Noise added to target policy during critic update
+parser.add_argument("--noise_clip", default=0.5, type=float)  # Range to clip target policy noise
+parser.add_argument("--policy_freq", default=2, type=int)  # Frequency of delayed policy updates
+parser.add_argument("-lr", default=3e-4, type=float, help="learning rate")
+
+args = parser.parse_args()
+
+
+def timer(start, end):
+    """ Helper to print training time """
+    hours, rem = divmod(end - start, 3600)
+    minutes, seconds = divmod(rem, 60)
+    print("\nTraining Time:  {:0>2}:{:0>2}:{:05.2f}".format(int(hours), int(minutes), seconds))
+
+
+def train():
+    rep = 0
+    rep_max = 500
+    eval_every = 1000
+    episode_reward = 0
+    i_episode = 1
+
+    state = env.reset(mode="train")
+
+    for frame in range(1, int(args.frames) + 1):
+        print(time.time())
+
+        rep += 1
+
+        if frame % eval_every == 0 or frame == 1:
+            eval_reward = test(env=env, agent=agent, args=args)
+            print("\neval_reward", eval_reward)
+
+        if args.type == "SAC":
+            action = agent.act(state)
+            next_state, reward, done, _ = env.step(action)
+            agent.step(state, action, reward, next_state, [done], frame, 0)
+            state = next_state
+
+        elif args.type == "TD3":
+            if frame < args.start_timesteps:
+                action = env.action_space.sample()
+            else:
+                action = (
+                        agent.select_action(np.array(state))
+                        + np.random.normal(0, max_action * args.expl_noise, size=action_size)
+                ).clip(-max_action, max_action)
+            next_state, reward, done, _ = env.step(action)
+            done_bool = float(done) if rep < rep_max else 0
+            replay_buffer.add(state, action, next_state, reward, done_bool)
+            state = next_state
+            if frame >= args.start_timesteps:
+                agent.train(replay_buffer, args.batch_size)
+
+        elif args.type == "PPO":
+            action = agent.select_action(state)
+            state, reward, done, _ = env.step(action)
+            agent.buffer.rewards.append(reward)
+            agent.buffer.is_terminals.append(done)
+            if frame % update_timestep == 0:
+                agent.update()
+            if frame % action_std_decay_freq == 0:
+                agent.decay_action_std(action_std_decay_rate, min_action_std)
+
+        episode_reward += reward
+
+        if done or rep >= rep_max:
+            rep = 0
+            print(f"Episode : {i_episode} \t\t Timestep : {frame} \t\t Episode Reward : {episode_reward}")
+            log_f.write('{},{},{}\n'.format(i_episode, frame, episode_reward))
+            log_f.flush()
+            i_episode += 1
+            episode_reward = 0
+            state = env.reset(mode="train")
+
+            if args.type == "SAC":
+                torch.save(agent.actor_local.state_dict(), checkpoint_path)
+            elif args.type == "TD3":
+                torch.save(agent.actor.state_dict(), checkpoint_path)
+            elif args.type == "PPO":
+                torch.save(agent.policy_old.state_dict(), checkpoint_path)
+
+
+if __name__ == "__main__":
+
+    env = Pendulum(args.render)
+
+    torch.manual_seed(args.seed)
+    np.random.seed(args.seed)
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print(f"Using device: {device}")
+
+    state_size = env.observation_space.shape[0]
+    action_size = env.action_space.shape[0]
+
+    if args.type == "SAC":
+        agent = Agent(state_size=state_size, action_size=action_size, args=args, device=device)
+
+    elif args.type == "TD3":
+        max_action = float(env.action_space.high[0])
+        kwargs = {
+            "state_dim": state_size,
+            "action_dim": action_size,
+            "max_action": max_action,
+            "discount": args.gamma,
+            "tau": args.tau,
+            "policy_noise": args.policy_noise * max_action,
+            "noise_clip": args.noise_clip * max_action,
+            "policy_freq": args.policy_freq,
+            "lr": args.lr
+        }
+        agent = TD3.TD3(**kwargs)
+        replay_buffer = utils.ReplayBuffer(state_size, action_size)
+
+    elif args.type == "PPO":
+        action_std = 0.6  # starting std for action distribution (Multivariate Normal)
+        action_std_decay_rate = 0.05  # linearly decay action_std (action_std = action_std - action_std_decay_rate)
+        min_action_std = 0.1  # minimum action_std (stop decay after action_std <= min_action_std)
+        action_std_decay_freq = args.frames / 10  # int(2.5e5)  # action_std decay frequency (in num timesteps)
+        update_timestep = 2000
+        K_epochs = 80
+        eps_clip = 0.2
+        gamma = args.gamma
+        lr_actor = args.lr_a
+        lr_critic = args.lr_c
+        agent = PPO(state_size, action_size, lr_actor, lr_critic, gamma, K_epochs, eps_clip, True, action_std)
+
+    ###################### logging ######################
+    log_dir = f"runs_{args.type}/rwip{args.trial}"
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+
+    checkpoint_path = log_dir + f"/rwip{args.trial}_{args.seed}.pth"
+
+    log_dir = log_dir + "/log"
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+
+    current_num_files = next(os.walk(log_dir))[2]
+    run_num = len(current_num_files)
+
+    log_f_name = log_dir + f"/{args.type}_log_{run_num}.csv"
+    log_f = open(log_f_name, "w+")
+    log_f.write('episode,timestep,raw_reward\n')
+    #####################################################
+
+    t0 = time.time()
+
+    train()
+
+    t1 = time.time()
+    timer(t0, t1)
+    env.close()
+    log_f.close()
