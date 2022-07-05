bd1932e27b8d9b6a2c6a68556f2a8d7de7c2cc82
drib861204 <56364972+drib861204@users.noreply.github.com>
2022-07-05 23:52:12 +0800

    0705 SAC env_dt 0.15, 0.2

--- try.sh
+++ try.sh
@@ -1,31 +1,29 @@
 #!/bin/sh
-# result and plot
-# 0703 PPO 11, 12
-# tune update timestep 1000, 4000
+# 0705 SAC env_dt 0.15, 0.2
 
-# python3 train.py -trial 16 -seed 0 -type SAC -env_dt 0.05
-# python3 train.py -trial 16 -seed 1 -type SAC -env_dt 0.05
-# python3 train.py -trial 16 -seed 2 -type SAC -env_dt 0.05
-# python3 train.py -trial 16 -seed 3 -type SAC -env_dt 0.05
-# python3 train.py -trial 16 -seed 4 -type SAC -env_dt 0.05
-# python3 plot_graph.py -trial 16 -type SAC
-# python3 test.py -trial 16 -seed 0 -type SAC -env_dt 0.05
-# python3 test.py -trial 16 -seed 1 -type SAC -env_dt 0.05
-# python3 test.py -trial 16 -seed 2 -type SAC -env_dt 0.05
-# python3 test.py -trial 16 -seed 3 -type SAC -env_dt 0.05
-# python3 test.py -trial 16 -seed 4 -type SAC -env_dt 0.05
+python3 train.py -trial 18 -seed 0 -type SAC -env_dt 0.15
+python3 train.py -trial 18 -seed 1 -type SAC -env_dt 0.15
+python3 train.py -trial 18 -seed 2 -type SAC -env_dt 0.15
+python3 train.py -trial 18 -seed 3 -type SAC -env_dt 0.15
+python3 train.py -trial 18 -seed 4 -type SAC -env_dt 0.15
+python3 plot_graph.py -trial 18 -type SAC
+python3 test.py -trial 18 -seed 0 -type SAC -env_dt 0.15
+python3 test.py -trial 18 -seed 1 -type SAC -env_dt 0.15
+python3 test.py -trial 18 -seed 2 -type SAC -env_dt 0.15
+python3 test.py -trial 18 -seed 3 -type SAC -env_dt 0.15
+python3 test.py -trial 18 -seed 4 -type SAC -env_dt 0.15
 
-# python3 train.py -trial 17 -seed 0 -type SAC -env_dt 0.05
-# python3 train.py -trial 17 -seed 1 -type SAC -env_dt 0.05
-# python3 train.py -trial 17 -seed 2 -type SAC -env_dt 0.05
-# python3 train.py -trial 17 -seed 3 -type SAC -env_dt 0.05
-# python3 train.py -trial 17 -seed 4 -type SAC -env_dt 0.05
-# python3 plot_graph.py -trial 17 -type SAC
-# python3 test.py -trial 17 -seed 0 -type SAC -env_dt 0.05
-# python3 test.py -trial 17 -seed 1 -type SAC -env_dt 0.05
-# python3 test.py -trial 17 -seed 2 -type SAC -env_dt 0.05
-# python3 test.py -trial 17 -seed 3 -type SAC -env_dt 0.05
-# python3 test.py -trial 17 -seed 4 -type SAC -env_dt 0.05
+python3 train.py -trial 19 -seed 0 -type SAC -env_dt 0.2
+python3 train.py -trial 19 -seed 1 -type SAC -env_dt 0.2
+python3 train.py -trial 19 -seed 2 -type SAC -env_dt 0.2
+python3 train.py -trial 19 -seed 3 -type SAC -env_dt 0.2
+python3 train.py -trial 19 -seed 4 -type SAC -env_dt 0.2
+python3 plot_graph.py -trial 19 -type SAC
+python3 test.py -trial 19 -seed 0 -type SAC -env_dt 0.2
+python3 test.py -trial 19 -seed 1 -type SAC -env_dt 0.2
+python3 test.py -trial 19 -seed 2 -type SAC -env_dt 0.2
+python3 test.py -trial 19 -seed 3 -type SAC -env_dt 0.2
+python3 test.py -trial 19 -seed 4 -type SAC -env_dt 0.2
 
 # python3 train.py -trial 24 -seed 0 -type TD3 -env_dt 0.05
 # python3 train.py -trial 24 -seed 1 -type TD3 -env_dt 0.05
@@ -51,26 +49,26 @@
 # python3 test.py -trial 25 -seed 3 -type TD3 -env_dt 0.05
 # python3 test.py -trial 25 -seed 4 -type TD3 -env_dt 0.05
 
-python3 train.py -trial 11 -seed 0 -type PPO -env_dt 0.05 -up_step 1000
-python3 train.py -trial 11 -seed 1 -type PPO -env_dt 0.05 -up_step 1000
-python3 train.py -trial 11 -seed 2 -type PPO -env_dt 0.05 -up_step 1000
-python3 train.py -trial 11 -seed 3 -type PPO -env_dt 0.05 -up_step 1000
-python3 train.py -trial 11 -seed 4 -type PPO -env_dt 0.05 -up_step 1000
-python3 plot_graph.py -trial 11 -type PPO
-python3 test.py -trial 11 -seed 0 -type PPO -env_dt 0.05
-python3 test.py -trial 11 -seed 1 -type PPO -env_dt 0.05
-python3 test.py -trial 11 -seed 2 -type PPO -env_dt 0.05
-python3 test.py -trial 11 -seed 3 -type PPO -env_dt 0.05
-python3 test.py -trial 11 -seed 4 -type PPO -env_dt 0.05
+#python3 train.py -trial 11 -seed 0 -type PPO -env_dt 0.05 -up_step 1000
+#python3 train.py -trial 11 -seed 1 -type PPO -env_dt 0.05 -up_step 1000
+#python3 train.py -trial 11 -seed 2 -type PPO -env_dt 0.05 -up_step 1000
+#python3 train.py -trial 11 -seed 3 -type PPO -env_dt 0.05 -up_step 1000
+#python3 train.py -trial 11 -seed 4 -type PPO -env_dt 0.05 -up_step 1000
+#python3 plot_graph.py -trial 11 -type PPO
+#python3 test.py -trial 11 -seed 0 -type PPO -env_dt 0.05
+#python3 test.py -trial 11 -seed 1 -type PPO -env_dt 0.05
+#python3 test.py -trial 11 -seed 2 -type PPO -env_dt 0.05
+#python3 test.py -trial 11 -seed 3 -type PPO -env_dt 0.05
+#python3 test.py -trial 11 -seed 4 -type PPO -env_dt 0.05
 
-python3 train.py -trial 12 -seed 0 -type PPO -env_dt 0.05 -up_step 4000
-python3 train.py -trial 12 -seed 1 -type PPO -env_dt 0.05 -up_step 4000
-python3 train.py -trial 12 -seed 2 -type PPO -env_dt 0.05 -up_step 4000
-python3 train.py -trial 12 -seed 3 -type PPO -env_dt 0.05 -up_step 4000
-python3 train.py -trial 12 -seed 4 -type PPO -env_dt 0.05 -up_step 4000
-python3 plot_graph.py -trial 12 -type PPO
-python3 test.py -trial 12 -seed 0 -type PPO -env_dt 0.05
-python3 test.py -trial 12 -seed 1 -type PPO -env_dt 0.05
-python3 test.py -trial 12 -seed 2 -type PPO -env_dt 0.05
-python3 test.py -trial 12 -seed 3 -type PPO -env_dt 0.05
-python3 test.py -trial 12 -seed 4 -type PPO -env_dt 0.05
+#python3 train.py -trial 12 -seed 0 -type PPO -env_dt 0.05 -up_step 4000
+#python3 train.py -trial 12 -seed 1 -type PPO -env_dt 0.05 -up_step 4000
+#python3 train.py -trial 12 -seed 2 -type PPO -env_dt 0.05 -up_step 4000
+#python3 train.py -trial 12 -seed 3 -type PPO -env_dt 0.05 -up_step 4000
+#python3 train.py -trial 12 -seed 4 -type PPO -env_dt 0.05 -up_step 4000
+#python3 plot_graph.py -trial 12 -type PPO
+#python3 test.py -trial 12 -seed 0 -type PPO -env_dt 0.05
+#python3 test.py -trial 12 -seed 1 -type PPO -env_dt 0.05
+#python3 test.py -trial 12 -seed 2 -type PPO -env_dt 0.05
+#python3 test.py -trial 12 -seed 3 -type PPO -env_dt 0.05
+#python3 test.py -trial 12 -seed 4 -type PPO -env_dt 0.05
