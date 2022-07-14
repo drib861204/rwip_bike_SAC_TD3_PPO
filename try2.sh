66b4808979c40ee565eae12250f09ffec3ef2bb9
drib861204 <56364972+drib861204@users.noreply.github.com>
2022-07-15 01:42:04 +0800

    SAC 78 validate SAC 34&50

--- try2.sh
+++ try2.sh
@@ -1,51 +1,9 @@
 #!/bin/sh
-# FIN: up_step 1000 kind of better than default 2000
-# PPO 27-30, tune up_step
+# SAC 78 validate SAC 34
 
-python3 train.py -trial 27 -seed 0 -type PPO -reward_function 1 -w_q2dot 0.001 --up_step 10
-python3 train.py -trial 27 -seed 1 -type PPO -reward_function 1 -w_q2dot 0.001 --up_step 10
-python3 train.py -trial 27 -seed 2 -type PPO -reward_function 1 -w_q2dot 0.001 --up_step 10
-python3 train.py -trial 27 -seed 3 -type PPO -reward_function 1 -w_q2dot 0.001 --up_step 10
-python3 train.py -trial 27 -seed 4 -type PPO -reward_function 1 -w_q2dot 0.001 --up_step 10
-python3 plot_graph.py -trial 27 -type PPO
-python3 test.py -trial 27 -seed 0 -type PPO -reward_function 1 -w_q2dot 0.001
-python3 test.py -trial 27 -seed 1 -type PPO -reward_function 1 -w_q2dot 0.001
-python3 test.py -trial 27 -seed 2 -type PPO -reward_function 1 -w_q2dot 0.001
-python3 test.py -trial 27 -seed 3 -type PPO -reward_function 1 -w_q2dot 0.001
-python3 test.py -trial 27 -seed 4 -type PPO -reward_function 1 -w_q2dot 0.001
-
-python3 train.py -trial 28 -seed 0 -type PPO -reward_function 1 -w_q2dot 0.001 --up_step 100
-python3 train.py -trial 28 -seed 1 -type PPO -reward_function 1 -w_q2dot 0.001 --up_step 100
-python3 train.py -trial 28 -seed 2 -type PPO -reward_function 1 -w_q2dot 0.001 --up_step 100
-python3 train.py -trial 28 -seed 3 -type PPO -reward_function 1 -w_q2dot 0.001 --up_step 100
-python3 train.py -trial 28 -seed 4 -type PPO -reward_function 1 -w_q2dot 0.001 --up_step 100
-python3 plot_graph.py -trial 28 -type PPO
-python3 test.py -trial 28 -seed 0 -type PPO -reward_function 1 -w_q2dot 0.001
-python3 test.py -trial 28 -seed 1 -type PPO -reward_function 1 -w_q2dot 0.001
-python3 test.py -trial 28 -seed 2 -type PPO -reward_function 1 -w_q2dot 0.001
-python3 test.py -trial 28 -seed 3 -type PPO -reward_function 1 -w_q2dot 0.001
-python3 test.py -trial 28 -seed 4 -type PPO -reward_function 1 -w_q2dot 0.001
-
-python3 train.py -trial 29 -seed 0 -type PPO -reward_function 1 -w_q2dot 0.001 --up_step 1000
-python3 train.py -trial 29 -seed 1 -type PPO -reward_function 1 -w_q2dot 0.001 --up_step 1000
-python3 train.py -trial 29 -seed 2 -type PPO -reward_function 1 -w_q2dot 0.001 --up_step 1000
-python3 train.py -trial 29 -seed 3 -type PPO -reward_function 1 -w_q2dot 0.001 --up_step 1000
-python3 train.py -trial 29 -seed 4 -type PPO -reward_function 1 -w_q2dot 0.001 --up_step 1000
-python3 plot_graph.py -trial 29 -type PPO
-python3 test.py -trial 29 -seed 0 -type PPO -reward_function 1 -w_q2dot 0.001
-python3 test.py -trial 29 -seed 1 -type PPO -reward_function 1 -w_q2dot 0.001
-python3 test.py -trial 29 -seed 2 -type PPO -reward_function 1 -w_q2dot 0.001
-python3 test.py -trial 29 -seed 3 -type PPO -reward_function 1 -w_q2dot 0.001
-python3 test.py -trial 29 -seed 4 -type PPO -reward_function 1 -w_q2dot 0.001
-
-python3 train.py -trial 30 -seed 0 -type PPO -reward_function 1 -w_q2dot 0.001 --up_step 10000
-python3 train.py -trial 30 -seed 1 -type PPO -reward_function 1 -w_q2dot 0.001 --up_step 10000
-python3 train.py -trial 30 -seed 2 -type PPO -reward_function 1 -w_q2dot 0.001 --up_step 10000
-python3 train.py -trial 30 -seed 3 -type PPO -reward_function 1 -w_q2dot 0.001 --up_step 10000
-python3 train.py -trial 30 -seed 4 -type PPO -reward_function 1 -w_q2dot 0.001 --up_step 10000
-python3 plot_graph.py -trial 30 -type PPO
-python3 test.py -trial 30 -seed 0 -type PPO -reward_function 1 -w_q2dot 0.001
-python3 test.py -trial 30 -seed 1 -type PPO -reward_function 1 -w_q2dot 0.001
-python3 test.py -trial 30 -seed 2 -type PPO -reward_function 1 -w_q2dot 0.001
-python3 test.py -trial 30 -seed 3 -type PPO -reward_function 1 -w_q2dot 0.001
-python3 test.py -trial 30 -seed 4 -type PPO -reward_function 1 -w_q2dot 0.001
+for i in 0 1 2 3 4
+do
+  python3 train2.py -trial 78 -seed $i -type SAC
+  python3 test2.py -trial 78 -seed $i -type SAC
+done
+python3 plot_graph.py -trial 78 -type SAC
