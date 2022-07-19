039a254b5a8c87b926d4ed906873a8f928fd4e70
drib861204 <56364972+drib861204@users.noreply.github.com>
2022-07-19 12:53:52 +0800

    SAC 97-98: tune replay_memory

--- try.sh
+++ try.sh
@@ -1,30 +1,16 @@
 #!/bin/sh
-# FIN: SAC 93-96: tune replay_memory
+# SAC 97-98: tune replay_memory
 
 for i in 0 1 2 3 4
 do
-  #python3 train.py -trial 93 -seed $i -type SAC -repm 50000
-  python3 test.py -trial 93 -seed $i -type SAC
+  python3 train.py -trial 97 -seed $i -type SAC -repm 500
+  python3 test.py -trial 97 -seed $i -type SAC
 done
-#python3 plot_graph.py -trial 93 -type SAC
+python3 plot_graph.py -trial 97 -type SAC
 
 for i in 0 1 2 3 4
 do
-  #python3 train.py -trial 94 -seed $i -type SAC -repm 10000
-  python3 test.py -trial 94 -seed $i -type SAC
+  python3 train.py -trial 98 -seed $i -type SAC -repm 100
+  python3 test.py -trial 98 -seed $i -type SAC
 done
-#python3 plot_graph.py -trial 94 -type SAC
-
-for i in 0 1 2 3 4
-do
-  #python3 train.py -trial 95 -seed $i -type SAC -repm 5000
-  python3 test.py -trial 95 -seed $i -type SAC
-done
-#python3 plot_graph.py -trial 95 -type SAC
-
-for i in 0 1 2 3 4
-do
-  #python3 train.py -trial 96 -seed $i -type SAC -repm 1000
-  python3 test.py -trial 96 -seed $i -type SAC
-done
-#python3 plot_graph.py -trial 96 -type SAC
+python3 plot_graph.py -trial 98 -type SAC
